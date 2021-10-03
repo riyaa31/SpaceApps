27d415083df7d2f93797b528c12234a7dd499d5b
riyaa31 <rshah@mun.ca>
2021-10-03 09:23:22 -0230

    NavBar

--- riskfree/src/App.js
+++ riskfree/src/App.js
@@ -1,22 +1,36 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { Typography } from '@material-ui/core';
+import './App.css';
+import * as ReactBootStrap from "react-bootstrap";
+
 
 function Homepage() {
   return(
-      <div className="w-full" style={{ paddingBottom: 100 }}>
-          <Helmet>
-              <title>Risk Free</title>
-          </Helmet>
-
-          <div className="flex flex-col md:flex-row sm:p-8 container">
-              <div className="flex flex-1 flex-col min-w-0">
-                  <Typography className="pl-24" variant="h4">
-                      Risk Free
-                  </Typography>
-                  </div>
-                  </div>
-                  </div>
+      <div className="App">
+         
+          <ReactBootStrap.Navbar collapseOnSelect expand="xl" bg="dark" variant="dark">
+  <ReactBootStrap.Navbar.Brand href="#home">RISKFREE</ReactBootStrap.Navbar.Brand>
+  <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
+    <ReactBootStrap.Nav className="me-auto">
+      <ReactBootStrap.Nav.Link href="#homepage">Homepage</ReactBootStrap.Nav.Link>
+      <ReactBootStrap.NavDropdown title="Predictor" id="collasible-nav-dropdown">
+        <ReactBootStrap.NavDropdown.Item href="#predictor/maps">Maps</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item href="#predictor/graphs">Graphs</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Divider />
+        <ReactBootStrap.NavDropdown.Item href="#predictor/links">Separated link</ReactBootStrap.NavDropdown.Item>
+      </ReactBootStrap.NavDropdown>
+    </ReactBootStrap.Nav>
+    <ReactBootStrap.Nav>
+      <ReactBootStrap.Nav.Link href="#future-data">Future Data</ReactBootStrap.Nav.Link>
+      <ReactBootStrap.Nav.Link eventKey={2} href="#about-us">
+        About Us
+      </ReactBootStrap.Nav.Link>
+    </ReactBootStrap.Nav>
+  </ReactBootStrap.Navbar.Collapse>
+  
+</ReactBootStrap.Navbar>
+         </div> 
   )
-}
+  }
 export default Homepage;
