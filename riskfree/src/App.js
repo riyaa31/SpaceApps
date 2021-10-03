222011929171d208d1c3b78bb9cb3e79aae262ae
riyaa31 <rshah@mun.ca>
2021-10-03 09:46:38 -0230

    chnages to navbar with links

--- riskfree/src/App.js
+++ riskfree/src/App.js
@@ -1,36 +1,43 @@
+
 import React from 'react';
-import { Helmet } from 'react-helmet';
 import './App.css';
 import * as ReactBootStrap from "react-bootstrap";
+import Aboutus from "./Components/Aboutus";
+import FutureData from "./Components/FutureData";
+import Homepage from "./Components/Homepage";
+import NavBar from "./Components/Navbar"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
-function Homepage() {
-  return(
-      <div className="App">
-         
-          <ReactBootStrap.Navbar collapseOnSelect expand="xl" bg="dark" variant="dark">
-  <ReactBootStrap.Navbar.Brand href="#home">RISKFREE</ReactBootStrap.Navbar.Brand>
-  <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
-    <ReactBootStrap.Nav className="me-auto">
-      <ReactBootStrap.Nav.Link href="#homepage">Homepage</ReactBootStrap.Nav.Link>
-      <ReactBootStrap.NavDropdown title="Predictor" id="collasible-nav-dropdown">
-        <ReactBootStrap.NavDropdown.Item href="#predictor/maps">Maps</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item href="#predictor/graphs">Graphs</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Divider />
-        <ReactBootStrap.NavDropdown.Item href="#predictor/links">Separated link</ReactBootStrap.NavDropdown.Item>
-      </ReactBootStrap.NavDropdown>
-    </ReactBootStrap.Nav>
-    <ReactBootStrap.Nav>
-      <ReactBootStrap.Nav.Link href="#future-data">Future Data</ReactBootStrap.Nav.Link>
-      <ReactBootStrap.Nav.Link eventKey={2} href="#about-us">
-        About Us
-      </ReactBootStrap.Nav.Link>
-    </ReactBootStrap.Nav>
-  </ReactBootStrap.Navbar.Collapse>
-  
-</ReactBootStrap.Navbar>
-         </div> 
-  )
-  }
-export default Homepage;
+function App() {
+  return (
+    <div className="App">
+        <Router>
+        <NavBar />
+ 
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/aboutus" component={Aboutus}>
+              <Aboutus />
+          </Route>
+          <Route path="/futuredata" component={FutureData}>
+            <FutureData />
+          </Route>
+          <Route path="/homepage" component={Homepage}>
+            <Homepage />
+          </Route>
+          
+        </Switch>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
--- riskfree/src/Components/Navbar.js
+++ riskfree/src/Components/Navbar.js
@@ -8,18 +8,17 @@ import {
 const NavBar = () => {
     return(
         <div className="App">
-    <ReactBootStrap.Navbar collapseOnSelect expand="xl" bg="primary" variant="dark">
+    <ReactBootStrap.Navbar collapseOnSelect expand="xl" bg="dark" variant="dark">
   <ReactBootStrap.Navbar.Brand href="#home">RISKFREE</ReactBootStrap.Navbar.Brand>
   <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
-    <ReactBootStrap.Nav className="mr-auto"> 
+    <ReactBootStrap.Nav className="me-auto"> 
     <Link to="/homepage">
     <ReactBootStrap.Nav.Link href="#homepage">Homepage</ReactBootStrap.Nav.Link>
     </Link>
-    
       <ReactBootStrap.NavDropdown title="Predictor" id="collasible-nav-dropdown">
-        <ReactBootStrap.NavDropdown.Item href="#action/3.1">Map</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item href="#action/3.2">Graphs</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item href="#maps">Maps</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item href="#graphs">Graphs</ReactBootStrap.NavDropdown.Item>
         <ReactBootStrap.NavDropdown.Divider />
         <ReactBootStrap.NavDropdown.Item href="#action/3.4">Separated link</ReactBootStrap.NavDropdown.Item>
       </ReactBootStrap.NavDropdown>
@@ -30,7 +29,7 @@ const NavBar = () => {
     </Link>
     <Link to="/aboutus">
     <ReactBootStrap.Nav.Link eventKey={2} href="#aboutus">
-        About Us
+        About Us 
       </ReactBootStrap.Nav.Link>
     </Link>
     </ReactBootStrap.Nav>
