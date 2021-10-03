158d417488a7f024d384a9668fc0e9dce309a58b
Riya Shah <72097261+riyaa31@users.noreply.github.com>
2021-10-03 08:28:54 -0230

    Create Aboutus.js

---
+++ riskfree/src/Components/Aboutus.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Aboutus = () => {
+    return(
+        <div className="App">
+            <h1>About Us</h1>
+        </div>
+    )
+}
+
+export default Aboutus;
