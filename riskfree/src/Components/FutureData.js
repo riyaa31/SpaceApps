f2e6db510a9f29973fec5bf8cb7c46c11218db11
Riya Shah <72097261+riyaa31@users.noreply.github.com>
2021-10-03 08:29:29 -0230

    Create FutureData.js

---
+++ riskfree/src/Components/FutureData.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FutureData = () => {
+    return(
+        <div className="App">
+            <h1>Future Data</h1>
+        </div>
+    )
+}
+
+export default FutureData;
