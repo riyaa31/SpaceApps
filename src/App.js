c7f7bf1a2a0cd998e35870d8d333437c65470a6a
riyaa31 <rshah@mun.ca>
2021-10-03 02:47:50 -0230

    updated file

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { Typography } from '@material-ui/core';
+
+function Homepage() {
+    return(
+        <div className="w-full" style={{ paddingBottom: 100 }}>
+            <Helmet>
+                <title>Risk Free</title>
+            </Helmet>
+
+            <div className="flex flex-col md:flex-row sm:p-8 container">
+                <div className="flex flex-1 flex-col min-w-0">
+                    <Typography className="pl-24" variant="h4">
+                        Risk Free App!!
+                    </Typography>
+                    </div>
+                    </div>
+                    </div>
+    )
+}
+export default Homepage
