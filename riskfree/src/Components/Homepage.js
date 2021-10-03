d5a50dd6572879d60ca45e3a7aa59012979c395a
Riya Shah <72097261+riyaa31@users.noreply.github.com>
2021-10-03 08:29:43 -0230

    Create Homepage.js

---
+++ riskfree/src/Components/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Feature = () => {
+    return(
+        <div className="App">
+            <h1>Homepage</h1>
+        </div>
+    )
+}
+
+export default Feature;
